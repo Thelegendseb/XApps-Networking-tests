228cad9a295e0f260e4da5032273c3ef91d69490
Thelegendseb <62959728+Thelegendseb@users.noreply.github.com>
2023-01-04 13:31:00 +0000

    Add files via upload

---
+++ FormClientXApps/FormClientXApps/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports XApps.Networking
+Public Class Form1
+    Dim C As XNetTransmitter
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        C = New XNetTransmitter
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' // Connect
+        C.ConnectSocket(InputBox("IP to connect to: "), 8888)
+        Button1.Text = "Connected"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' // Start
+        C.Start()
+        Button2.Text = "Started"
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' // Send
+        C.Send(System.Text.Encoding.UTF8.GetBytes(InputBox("Type your message to send:")))
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ' // Stop
+        C.Stop()
+    End Sub
+End Class
---
+++ FormServerXApps/FormServerXApps/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Net.Sockets
+Imports XApps.Networking
+Public Class Form1
+    Dim S As XNetServer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        S = New XNetServer
+        S.BindSocket(InputBox("IP to bind to: "), 8888)
+        AddHandler S.ClientReceived, AddressOf OnClientReceived
+        S.Start()
+    End Sub
+    Private Sub OnClientReceived(SocketFound As Socket)
+        ' // Create XNetTransmitter instance from Socket
+
+        MsgBox("Client received - Local Endpoint: " & SocketFound.LocalEndPoint.ToString)
+        MsgBox("Client received - Remote Endpoint: " & SocketFound.RemoteEndPoint.ToString)
+        Dim Transmitter As New XNetTransmitter(SocketFound)
+        Transmitter.Start()
+        AddHandler Transmitter.DataReceived, AddressOf OnConnectedSocketDataReceived
+
+    End Sub
+    Private Sub OnConnectedSocketDataReceived(data() As Byte)
+        Dim str As String = System.Text.Encoding.UTF8.GetString(data).Trim()
+        str = str.Replace(vbNullChar, "")
+        Me.RichTextBox1.Invoke(Sub() Me.RichTextBox1.Text &= str & vbCrLf)
+    End Sub
+End Class
+
+' // =====================================
